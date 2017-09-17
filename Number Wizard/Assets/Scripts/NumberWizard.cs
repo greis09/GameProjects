4d73bc82e3171e70fc25514cec137aca75037106
Gabriel Reis <gbueno@ciandt.com>
2017-09-17 18:44:37 -0300

    Projetos iniciais de estudo

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head,  but dont tell me!");
+		
+		var max = 1000;
+		var min = 1;
+		
+		print("Pick a number between "+min+" and "+max);
+		
+		print("Is the number higher or lower than 500?");
+		print("Press Up arrow for higher, down arrow for lower and return for equal");
+			
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			print("Up arrow pressed");
+		}
+		
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			print("Down arrow pressed");
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			print("The number is 500");
+		}
+	
+	}
+}
