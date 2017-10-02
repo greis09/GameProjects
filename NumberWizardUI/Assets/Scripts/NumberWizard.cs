64a199331cca6b4eb34cfcc7d9bc184580dbafd5
Gabriel Bueno Reis <gbueno@cit>
2017-10-02 08:17:35 -0300

    Final do NumberWizardUI

---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max = 100000;
+	int min = 1;
+	int guess = 50000;
+	int maxGuesses = 20;
+
+	public Text text;
+
+	void Start () 
+	{
+		text.text = guess.ToString();
+		max ++;	
+	}
+	
+	public void GuessHigher()
+	{
+		min = guess;
+		adjustGuess();
+	}
+
+	public void GuessLower()
+	{
+		max = guess;
+		adjustGuess();
+	}
+
+	void adjustGuess()
+	{
+		guess = (max+min)/2;
+		text.text = guess.ToString();
+		maxGuesses --;
+		if(maxGuesses == 0)
+		{
+			Application.LoadLevel("Win");
+		}
+	}
+}
